d6959636820af3c27151c589eb5dfcd22dc30711
palcainoj <paalcaino@gmail.com>
2024-01-18 16:43:07 -0300

    finish app

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -6,7 +6,6 @@ import {
 } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
-import events from "events";
 import Image from "next/image";
 
 const EventDetails = async ({
@@ -14,6 +13,7 @@ const EventDetails = async ({
   searchParams,
 }: SearchParamProps) => {
   const event = await getEventById(id);
+
   const relatedEvents = await getRelatedEventsByCategory({
     categoryId: event.category._id,
     eventId: event._id,
@@ -35,6 +35,7 @@ const EventDetails = async ({
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
               <h2 className="h2-bold">{event.title}</h2>
+
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
@@ -44,6 +45,7 @@ const EventDetails = async ({
                     {event.category.name}
                   </p>
                 </div>
+
                 <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
                   by{" "}
                   <span className="text-primary-500">
@@ -52,7 +54,7 @@ const EventDetails = async ({
                 </p>
               </div>
             </div>
-            {/* CHECKOUT BUTTON */}
+
             <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
@@ -61,7 +63,7 @@ const EventDetails = async ({
                   src="/assets/icons/calendar.svg"
                   alt="calendar"
                   width={32}
-                  height={30}
+                  height={32}
                 />
                 <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                   <p>
@@ -74,6 +76,7 @@ const EventDetails = async ({
                   </p>
                 </div>
               </div>
+
               <div className="p-regular-20 flex items-center gap-3">
                 <Image
                   src="/assets/icons/location.svg"
@@ -84,8 +87,9 @@ const EventDetails = async ({
                 <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
               </div>
             </div>
+
             <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">What you'll learn</p>
+              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
               <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
               <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
                 {event.url}
@@ -95,16 +99,18 @@ const EventDetails = async ({
         </div>
       </section>
 
+      {/* EVENTS with the same category */}
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Related Events</h2>
+
         <Collection
           data={relatedEvents?.data}
-          emptyTitle="No events found"
+          emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
-          totalPages={2}
+          limit={3}
+          page={searchParams.page as string}
+          totalPages={relatedEvents?.totalPages}
         />
       </section>
     </>
